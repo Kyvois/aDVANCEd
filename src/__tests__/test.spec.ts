817253db05acdaccef53c9dc308e59938e5c05aa
Kyvois <98109484+Kyvois@users.noreply.github.com>
2023-01-13 13:27:51 +0000

    dsds

--- src/__tests__/test.spec.ts
+++ src/__tests__/test.spec.ts
@@ -102,3 +102,33 @@ it('finds a bearer token in headers under "authorization: <anykey>" and sets it 
   const app = setup({ headerKey: 'test' });
 
   let requestToken;
+  app.use((ctx) => {
+    requestToken = ctx.request.token;
+  });
+
+  await request(app.callback()).get('/').set('Authorization', `test ${token}`);
+
+  expect(requestToken).toBe(token);
+});
+
+it('finds a bearer token in header SIGNED cookies[<anykey>] and sets it to req.token', async () => {
+  const app = setup({ cookie: { key: 'test', signed: true, secret } });
+
+  let requestToken;
+  app.use((ctx) => {
+    requestToken = ctx.request.token;
+  });
+
+  // simulate the res.cookie signed prefix 's:'
+  const signedCookie = encodeURI(`s:${cookie.sign(token, secret)}`);
+
+  await request(app.callback()).get('/').set('Cookie', `test=${signedCookie};`);
+
+  expect(requestToken).toBe(token);
+});
+
+it('finds a bearer token in header NON SIGNED cookies[<anykey>] and sets it to request.token', async () => {
+  const app = setup({ cookie: { key: 'test' } });
+
+  let requestToken;
+  app.use((ctx) => {
