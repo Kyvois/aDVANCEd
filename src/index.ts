ad39c6acda775e969432edee9ae8a06b468dd3fc
Kyvois <98109484+Kyvois@users.noreply.github.com>
2023-01-15 16:14:51 +0000

    Fix design

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,31 @@ export type CookieOptions = (
   | {
       signed?: true;
       secret: string | string[];
+    }
+  | {
+      signed?: false;
+      secret?: string | string[];
+    }
+) & { key?: string };
+
+export type Options = {
+  queryKey?: string;
+  bodyKey?: string;
+  headerKey?: string;
+  reqKey?: string;
+  cookie?: CookieOptions | false;
+};
+
+const getCookie = (serializedCookies: string, key: string) =>
+  cookieModule.parse(serializedCookies)[key] ?? false;
+
+export function bearerToken<
+  StateT = Koa.DefaultState,
+  ContextT = Koa.DefaultContext,
+>(opts: Options = {}): Koa.Middleware<StateT, ContextT> {
+  const queryKey = opts.queryKey ?? 'access_token';
+  const bodyKey = opts.bodyKey ?? 'access_token';
+  const headerKey = opts.headerKey ?? 'Bearer';
+  const reqKey = opts.reqKey ?? 'token';
+  const cookie = opts.cookie ?? false;
+  const cookieKey = cookie && cookie.key ? cookie.key : 'access_token';
